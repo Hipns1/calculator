3ba87ca5503961469582c8c9a026db23bfe0d814
Hipns1 <jesudpf21@hotmail.com>
2022-01-08 20:29:52 -0500

    Estructura HTML

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,33 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>My calculator with React.js</h1>
+      <div className="calculator-grid">
+
+        <div className="output">
+          <div className="previous-operand"></div>
+          <div className="current-operand"></div>
+        </div>
+
+        <button className="spand-two">AC</button>
+        <button>DEL</button>
+        <button>/</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>X</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>+</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>-</button>
+        <button>.</button>
+        <button>0</button>
+        <button className="spand-two">=</button>
+
+      </div>
     </div>
   );
 }
